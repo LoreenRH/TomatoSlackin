2daf4445882226c88bdcb7b70b4d3a0d2e1d979c
meo84 <oopsiday@gmail.com>
2017-04-19 15:00:19 +0200

    bot starts 5mn pomodoro session and counts down til the end

--- thomas_magnum.rb
+++ thomas_magnum.rb
@@ -1,12 +1,24 @@
 require 'slack-ruby-bot'
+require 'timers'
 
 class ThomasBot < SlackRubyBot::Bot
-	puts 'coucou 1'
-  command 'pomodoro' do |client, data, match|
-  	puts 'coucou 2'
-    client.say(text: 'Hey there ;) !', channel: data.channel)
+  command 'pomodoro' do |client, data, _match|
+    client.say(text: "<@#{data.user}> just started a Pomodoro session!", channel: data.channel)
+    timers = Timers::Group.new
+
+    minutes_duration = 5
+    minutes_left = 5
+
+    pomodoro_timer = timers.now_and_every(60) do
+      message = (minutes_left == 0 ? "<@#{data.user}> just finished a Pomodoro session!" : minutes_left.to_s + ' minutes left!' )
+      client.say(text: message, channel: data.channel)
+    end
+
+    minutes_duration.times do
+      minutes_left -= 1
+      timers.wait
+    end
   end
 end
 
-puts 'coucou'
 ThomasBot.run
