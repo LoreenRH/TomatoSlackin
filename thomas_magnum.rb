31f76fb7d70ccc327a0fd2f4d7bcbd8b9334ea0c
meo84 <oopsiday@gmail.com>
2017-04-21 10:58:43 +0200

    bot starts 6sec pomodoro session and alerts the end

--- thomas_magnum.rb
+++ thomas_magnum.rb
@@ -6,18 +6,14 @@ class ThomasBot < SlackRubyBot::Bot
     client.say(text: "<@#{data.user}> just started a Pomodoro session!", channel: data.channel)
     timers = Timers::Group.new
 
-    minutes_duration = 5
-    minutes_left = 5
+    minutes_in_pomodoro_session = 0.10
+    seconds_in_pomodoro_session = minutes_in_pomodoro_session * 60
 
-    pomodoro_timer = timers.now_and_every(60) do
-      message = (minutes_left == 0 ? "<@#{data.user}> just finished a Pomodoro session!" : minutes_left.to_s + ' minutes left!' )
-      client.say(text: message, channel: data.channel)
+    pomodoro_timer = timers.after(seconds_in_pomodoro_session) do
+      client.say(text: "<@#{data.user}> just finished a Pomodoro session!", channel: data.channel)
     end
 
-    minutes_duration.times do
-      minutes_left -= 1
-      timers.wait
-    end
+    timers.wait
   end
 end
 
